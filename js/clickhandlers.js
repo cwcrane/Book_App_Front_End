baa2b9e0639946f1f3ae4e44a2cb41ffec23f465
cwcrane <ccrane@excelsiorintegrated.com>
2015-11-08 16:46:03 -0500

    Incomplete: register clickhandler. Made some progress on registration clickhandler. It is not functioning yet in this commit.

---
+++ js/clickhandlers.js
@@ -0,0 +1,44 @@
+'use strict';
+
+$(document).ready(
+
+  $(function() {
+    //returns data entered in form, as an object
+    var form2object = function(form) {
+      var data = {};
+      $(form).find("input").each(function(index, element) {
+        var type = $(this).attr('type');
+        if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+          data[$(this).attr('name')] = $(this).val();
+        }
+      });
+      return data;
+    };
+
+    var wrap = function wrap(root, formData) {
+      var wrapper = {};
+      wrapper[root] = formData;
+      return wrapper;
+    };
+
+    var callback = function callback(error, data) {
+      if (error) {
+        console.error(error);
+        //$('#result').val('status: ' + error.status + ', error: ' +error.error);
+        return;
+      }
+      console.log(data);
+      //$('#result').val(JSON.stringify(data, null, 4));
+    };
+
+    $('#registerForm').on('submit', function(e) {
+      var credentials = wrap('credentials', form2object(this));
+      books_api.register(credentials, callback)
+      // tttapi.register(credentials, callback);
+      // e.preventDefault();
+    });
+
+
+
+);
+
