7abf982c301bc697a99615f5dab686f6d62adb58
cwcrane <ccrane@excelsiorintegrated.com>
2015-11-09 13:05:14 -0500

    Added UX for Register Process. Created a ux.js file to store user experience actions. Implemented basic show hide features for the login modal window when a user first enters the site. Next step is to implement login clickhandlers.

--- js/clickhandlers.js
+++ js/clickhandlers.js
@@ -1,46 +1,70 @@
 'use strict';
 
-$(document).ready(
+$(document).ready(function() {
 
-  $(function() {
+  uxState();
 
-    var form2object = function(form) {
-      var data = {};
-      $(form).find("input").each(function(index, element) {
-        var type = $(this).attr('type');
-        if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
-          data[$(this).attr('name')] = $(this).val();
-        }
-      });
-      return data;
-    };
-
-    var wrap = function wrap(root, formData) {
-      var wrapper = {};
-      wrapper[root] = formData;
-      return wrapper;
-    };
-
-    var callback = function callback(error, data) {
-      if (error) {
-        console.log('status: ' + error.status + ', error: ' +error.error)
-      } else {console.log(data)};
-    };
-
-    $('#registerForm').on('submit', function(e) {
-      var credentials = wrap('credentials', form2object(this));
-      bookApi.register(credentials, callback);
-      e.preventDefault();
+  var form2object = function(form) {
+    var data = {};
+    $(form).find("input").each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
     });
+    return data;
+  };
+
+  var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+//REGISTER CLICKHANDLER
+
+  var registerCallback = function(error, data) {
+    if (error) {
+      $("#RegisterMessage").html("An error occured, please try again.")
+      //console.log('status: ' + error.status + ', error: ' +error.error)
+    } else if (data) {
+      $("#RegisterMessage").html("Registration successful, please log in.")
+      console.log(data);
+    };
+  };
+
+  $('#registerForm').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    bookApi.register(credentials, registerCallback);
+    e.preventDefault();
+  });
 
 
-    $('#loginForm').on('submit', function(e){
-      var credentials = wrap('credentials', form2object(this));
-      bookApi.login(credentials, callback);
-      e.preventDefault();
-    });
+//LOGIN CLICKHANDLER
 
+  var loginCallback = function(error, data) {
+    if (error) {
+      $("#LoginMessage").html("Error occured, please try again.")
+      console.log('status: ' + error.status + ', error: ' +error.error)
+    } else if (data){
+      console.log(data);
+      currentUser.loggedIn = true;
+      currentUser.user = data.user
+      uxState();
+    };
+  };
 
-  })
-);
+  $('#loginForm').on('submit', function(e){
+    var credentials = wrap('credentials', form2object(this));
+    bookApi.login(credentials, callback);
+    e.preventDefault();
+  });
+
+  $("#skipLogIn").on('click', function(e){
+    currentUser.skipLogIn = true;
+    uxState();
+    e.preventDefault();
+  });
+
+});
 
---
+++ js/ux.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var currentUser = {
+    loggedIn: false,
+    skipLogIn: false,
+    user: {},
+  };
+
+  var beforeLoginView = function(){
+    $('#start-hide1').hide();
+    $('#start-hide2').hide();
+    $('#start-hide3').hide();
+    $('#login-overlay').show();
+  };
+
+  var afterLoginView = function(){
+    $('#start-hide1').show();
+    $('#start-hide2').show();
+    $('#start-hide3').show();
+    $('#login-overlay').hide();
+  };
+
+  var skipLoginView = function(){
+    $('#start-hide1').show();
+    $('#start-hide2').show();
+    $('#start-hide3').show();
+    $('#login-overlay').hide();
+  };
+
+  var uxState = function (){
+    if (currentUser.loggedIn){
+      return afterLoginView();
+    }else if (currentUser.skipLogIn){
+      return skipLoginView();
+    }else beforeLoginView();
+  };
