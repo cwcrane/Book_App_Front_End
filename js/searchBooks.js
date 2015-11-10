cc414a646ea18bf3c05273b4cbd2a1dbe57da430
cwcrane <ccrane@excelsiorintegrated.com>
2015-11-09 19:38:11 -0500

    Add "on Loan" button to showBooks.
    Created a handlebars helper to display either a green button or yellow button depending on whether a book is available or on loan.

--- js/searchBooks.js
+++ js/searchBooks.js
@@ -2,12 +2,20 @@
 
 //LOAD BOOKS
 
+  Handlebars.registerHelper('ifOnLoan', function (conditionalVariable, options){
+    if (conditionalVariable === options.hash.value) {
+      return options.fn(this)
+    } else {
+      return options.inverse(this);
+    }
+  });
+
   var bookTemplate = Handlebars.compile($('#book-index').html());
 
   var bookCallback = function(error, data){
     if (error){console.log(error);
     }else {
-      //console.log(data);
+      console.log(data.books);
       var rowHTML = bookTemplate({books: data.books});
       $("#books").html(rowHTML);
     }
